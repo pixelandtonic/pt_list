69ff87f137141e953913f1d244965a187b38d135
Brandon Kelly <me@brandon-kelly.com>
2010-12-14 12:53:31 -0800

    1.0.3

--- ee1/fieldtypes/pt_list/ft.pt_list.php
+++ ee1/fieldtypes/pt_list/ft.pt_list.php
@@ -12,7 +12,7 @@ class Pt_list extends Fieldframe_Fieldtype {
 
 	var $info = array(
 		'name'             => 'P&T List',
-		'version'          => '1.0.2',
+		'version'          => '1.0.3',
 		'versions_xml_url' => 'http://pixelandtonic.com/ee/versions.xml',
 		'no_lang'          => TRUE
 	);
